364034c09733a85d0f26d422d9ad12a7969d58ac
Legolas <geran.vanatta@gmail.com>
2022-03-10 17:45:05 -0600

    connected to SQL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mySQL username
+        user: 'root',
+        // your mysql password
+        password: 'cassie1020',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not found)
 app.use((req, res) => {
